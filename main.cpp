b4b85bc0839777fdb3154b125cceecce7c5e02b2
Alice (StomatyVD) <caplin.vlad2018@yandex.ru>
2024-07-05 20:55:47 +0300

    code: вывод результата деления А на В

--- main.cpp
+++ main.cpp
@@ -8,5 +8,8 @@ int main()
     int a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
-         << "A - B = " << a - b << '\n';
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / b = " << a / b << '\n';
+
 }
